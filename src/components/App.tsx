10ce7a3ae5474402e5c7ca7dfd0ed715b6985cd2
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-02-20 16:24:05 +0200

    connect react and redux fix problem with initial state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import {Provider} from 'react-redux'
+import {store} from '../state'
+import RepositoryList from './RepositoryList'
 const App = () => {
   return (
-    <div>
-      Hi!
-    </div>
+    <Provider store={store}>
+      <div>
+        <h1>Search for a repository</h1>
+        <RepositoryList/>
+      </div>
+    </Provider>
   )
 }
 
---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RepositoryList: React.FC = () => {
+  return (
+    <form>
+      <input></input>
+      <button>Search</button>
+    </form>
+  )
+}
+
+export default RepositoryList
